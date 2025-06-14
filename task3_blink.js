453e05a9d4fe92e9724cf2182db426bc54aaf54b
BhavyaYeluri-9395 <bhavya.23bce9395@vitapstudent.ac.in>
2025-06-14 19:26:45 +0530

    Update task3_blink.js

--- task3_blink.js
+++ task3_blink.js
@@ -1,9 +1,9 @@
 let x = document.getElementById("mypic");
 
 x.onmouseover = () => {
-  x.src = "./gwen_blink.jpg";
+  x.src = "./gwen_close.png";
 };
 
 x.onmouseout = () => {
-  x.src = "./gwen_open.jpg";
+  x.src = "./gwen_open.png";
 };
